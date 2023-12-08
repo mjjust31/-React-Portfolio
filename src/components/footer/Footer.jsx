b1588b4c526b5b149162630bf0a55a9f775bf10e
justmann <mj.justmann@gmail.com>
2023-12-08 13:29:54 -0600

    footer-added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,11 @@
+import "./Footer.scss";
+
 function Footer() {
   return (
-    <div>
+    <div className="custom-footer">
+      <h1>This will be the footer</h1>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
