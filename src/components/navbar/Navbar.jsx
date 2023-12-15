239e3dac983b9c651d75f0d84074158a23749e72
justmann <mj.justmann@gmail.com>
2023-12-14 23:01:07 -0600

    link-updates

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./Navbar.scss";
 import Nav from "react-bootstrap/Nav";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [activeKey, setActiveKey] = useState("/");
@@ -11,7 +12,7 @@ function Navbar() {
 
   return (
     <>
-      <div className=" custom-background text-center p-3">Jessica Justmann</div>
+      <div className="custom-background text-center p-3">Jessica Justmann</div>
       <Nav
         className="custom-navbar"
         justify
@@ -19,22 +20,22 @@ function Navbar() {
         activeKey={activeKey}
         onSelect={handleSelect}>
         <Nav.Item>
-          <Nav.Link href="/" eventKey="/">
+          <Nav.Link as={Link} to="/" eventKey="/">
             About Me
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/portfolio" eventKey="/portfolio">
+          <Nav.Link as={Link} to="/portfolio" eventKey="/portfolio">
             Portfolio
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/contact-me" eventKey="/contact-me">
+          <Nav.Link as={Link} to="/contact-me" eventKey="/contact-me">
             Contact
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/resume" eventKey="/resume">
+          <Nav.Link as={Link} to="/resume" eventKey="/resume">
             Resume
           </Nav.Link>
         </Nav.Item>
